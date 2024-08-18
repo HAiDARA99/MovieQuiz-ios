7c9c052421bb8565503f50276862cbeff054a361
Raul Ismagilov <smaggy@mail.ru>
2024-08-18 18:53:20 +0300

    Renamed hello world

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -6,11 +6,10 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        print("hello world for branch to merge")
+        sleep(3)
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
 
     func application(
         _ application: UIApplication,
